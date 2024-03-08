fda59c8fef6f7ca10485329593040dab4e5842a8
Caleb Fultz <caleb@fultz.dev>
2024-03-07 23:09:19 -0500

    Working on double sided cards

--- main.py
+++ main.py
@@ -21,14 +21,35 @@ input = Entry(width=150)
 input.pack(side=TOP)
 
 
+def flip_card():
+    random_mtg_card = Image.open("reverse.jpg").resize((388,580))
+    new_mtg_card = ImageTk.PhotoImage(random_mtg_card)
+    card_placeholder.configure(image=new_mtg_card)
+    card_placeholder.image = new_mtg_card
+
 def search_clicked():
     new_search = input.get()
     print(new_search)
     card = loads(get(f"https://api.scryfall.com/cards/named?fuzzy={new_search}").text)
-    img_url = card['image_uris']['large']
     mtg_title = card['name']
-    with open('image.jpg', 'wb') as out_file:
-     copyfileobj(get(img_url, stream = True).raw, out_file)
+    try:
+        img_url  = card['image_uris']['large']
+    except:
+        img_url  = card['card_faces'][0]['image_uris']['large']
+        with open('image.jpg', 'wb') as out_file:
+            copyfileobj(get(img_url , stream = True).raw, out_file)
+
+        img_url2 = card['card_faces'][1]['image_uris']['large']
+        with open('reverse.jpg', 'wb') as out_file:
+            copyfileobj(get(img_url2, stream = True).raw, out_file)
+        
+        search = tkinter.Button(text="Flip", command=flip_card)
+        search.pack(side=RIGHT)
+
+    else:
+        with open('image.jpg', 'wb') as out_file:
+            copyfileobj(get(img_url, stream = True).raw, out_file)
+        
 
     random_mtg_card = Image.open("image.jpg").resize((388,580))
     new_mtg_card = ImageTk.PhotoImage(random_mtg_card)
